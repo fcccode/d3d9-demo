59df7624abdfe810b1c349491895e9fc937b843b
zauonlok <zauonlok@gmail.com>
2017-02-18 16:41:14 +0800

    Add empty application

---
+++ application.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <d3d9.h>
+#include <d3dx9.h>
+
+#include "framework.h"
+
+void on_setup(config *cfg) {
+    cfg->title = "Demo";
+}
+
+void on_ready() {
+}
+
+void on_loss() {
+}
+
+void on_reset(int width, int height) {
+    printf("%d, %d\n", width, height);
+}
+
+void on_render(int dtime) {
+    g_direct3d->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffeeeeee, 1.0f, 0);
+}
